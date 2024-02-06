6e2dd22db1cb4047615b2616ac6ec4c9809c0674
Vinicius Magalhaes <vinizipi@gmail.com>
2024-02-05 22:29:50 -0300

    Atualização de routes

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -4,18 +4,11 @@ import Home from "../Home/page";
 import Login from "../Login/page";
 import Registro from "../Registro/page";
 
-const Private = ({ Item }) => {
-    const signed = false;
-
-    return signed > 0 ? <Item /> : <Login />
-
-}
-
 const RoutesApp = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/Home" element={<Private Item={Home} />} />
+                <Route path="/Home" element={<Home />} />
                 <Route path="/Login" element={<Login />} />
                 <Route path="/Registro" element={<Registro />} />
             </Routes>
